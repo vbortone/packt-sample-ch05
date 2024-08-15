3b0e599edac62c0a4ae2d95d5e788a74b48bc479
Vincent Bortone <vincent.bortone@pwc.com>
2024-08-15 00:51:45 -0400

    feat: update code

--- backend/src/services/posts.js
+++ backend/src/services/posts.js
@@ -1,7 +1,7 @@
 import { Post } from '../db/models/post.js';
 
-export async function createPost({ title, author, content, tags }) {
-  const post = new Post({ title, author, content, tags });
+export async function createPost({ title, author, contents, tags }) {
+  const post = new Post({ title, author, contents, tags });
   return await post.save();
 }
 
